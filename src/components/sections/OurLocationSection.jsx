ca16d050aab3f5cbd7b952c2679d24f55d69dbfc
zaidelayan77-star <zaidelayan77@gmail.com>
2026-03-15 09:57:55 +0300

    edit

--- src/components/sections/OurLocationSection.jsx
+++ src/components/sections/OurLocationSection.jsx
@@ -33,7 +33,7 @@ export default function OurLocationSection() {
                     className="w-full h-[450px] rounded-xl overflow-hidden shadow-lg border border-gray-100"
                 >
                     <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d58156.973955615784!2d54.49884594248439!3d24.34863870624022!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5e41e3fc45145b%3A0x6b6ce7a23c21a5f6!2sMusaffah%20-%20Abu%20Dhabi!5e0!3m2!1sen!2sae!4v1709664580214!5m2!1sen!2sae"
+                        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3634.7370668763347!2d54.504865285007014!3d24.355650084294425!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjTCsDIxJzIwLjMiTiA1NMKwMzAnMDkuNiJF!5e0!3m2!1sar!2sjo!4v1773557729364!5m2!1sar!2sjo"
                         width="100%"
                         height="100%"
                         style={{ border: 0 }}
@@ -46,4 +46,4 @@ export default function OurLocationSection() {
             </div>
         </section>
     );
-}
+}
